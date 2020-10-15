a5b17e624c7209d2762af1e524efb7c8c3ad31d1
mtchristian <matheuschristian667@gmail.com>
2020-10-14 23:27:40 -0300

    Primeiro commite do código

---
+++ input-output.js
@@ -0,0 +1,5 @@
+var nome, nota; 
+nome = prompt("Digite o seu nome: ")
+nota = prompt("Digite a sua nota: ")
+alert ("O nome digitado foi: " + nome)
+alert ("A nota inserida foi: " + nota)
---
+++ operadores-relacionais.js
@@ -0,0 +1,24 @@
+/*
+   nota01, nota02: real
+
+Inicio
+// Seção de Comandos, procedimento, funções, operadores, etc...
+   nota01 := 10
+   nota02 := 15
+
+   se (nota01 <>  nota02) entao
+      escreval ("Nota 1 é diferente!")
+   senao
+        escreval ("Nota 1 é igual!")
+   fimse
+*/
+
+var nota01, nota02;
+
+nota01 = 10
+nota02 = 15
+
+if (nota01 != nota02)
+    alert("Nota 1 é diferente!")
+else
+    alert("Nota 1 é igual!")
